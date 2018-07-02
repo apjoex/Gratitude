54d79f7c1c337e29b73ffcff23b11327f91557fd
apjoex <apjoex@gmail.com>
2018-07-02 18:39:09 +0100

    ++Added app icon ++Added SplashScreen ++Added custom font

--- app/src/main/java/com/apjoex/gratitude/SplashScreen.kt
+++ app/src/main/java/com/apjoex/gratitude/SplashScreen.kt
@@ -2,11 +2,17 @@ package com.apjoex.gratitude
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash_screen)
+
+        logo.startAnimation(AnimationUtils.loadAnimation(self@this, R.anim.zoom_in))
     }
 }
